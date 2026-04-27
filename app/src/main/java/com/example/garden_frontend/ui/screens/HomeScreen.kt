5bc49973148727f74d2ae735df1a2d34c48474ce
vlad595 <vladggaort@gmail.com>
2026-04-27 19:57:38 +0300

    Added pages and pager scroll to home screen

--- app/src/main/java/com/example/garden_frontend/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/garden_frontend/ui/screens/HomeScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,24 +22,43 @@ import com.example.garden_frontend.ui.components.BottomBar
 import com.example.garden_frontend.ui.components.TopBar
 import com.example.garden_frontend.ui.navigation.Screen
 import com.example.garden_frontend.ui.theme.GardenfrontendTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun Main(navController: NavHostController){
+    val pagerState = rememberPagerState(pageCount = { 4 })
+
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             TopBar(iconOnClick = {navController.navigate(Screen.Account.route)}, painterResource(R.drawable.account_circle_24px))
         },
 
         bottomBar = {
-            BottomBar()
+            BottomBar(currentPage = pagerState.currentPage, onTabSelected = { index ->
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(index)
+                }
+            })
         }
     ) { innerPadding ->
-        HomeScreen(modifier = Modifier.padding(innerPadding))
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        ) { page ->
+            when (page){
+                0 -> HomeScreen()
+                1 -> Text("Мій садок")
+                2 -> Text("Врожай")
+                3 -> Text("Засоби догляду")
+            }
+        }
     }
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier){
+fun HomeScreen(modifier: Modifier = Modifier){
     Column(
 
     ) {
